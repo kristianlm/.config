580fc2da3b780c42f9162eb1506709f4ec5eadb7
Kristian Lein-Mathisen <kristian@adellica.com>
2018-04-09 09:59:46 +0200

    emacs: dim default face
    because I don't like it when it's too bright

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -51,4 +51,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#232323" :foreground "#E6E1DC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "ADBO" :family "Source Code Pro")))))
+ '(default ((t (:inherit nil :stipple nil :background "#232323" :foreground "#c6c1cC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "ADBO" :family "Source Code Pro")))))
