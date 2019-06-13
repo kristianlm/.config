ab484febb49c340860b5977e803732eda637f52c
Kristian Lein-Mathisen <kristian@adellica.com>
2019-06-13 19:08:51 +0200

    fish: styling cmd_duration

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,5 +1,6 @@
+# Defined in - @ line 2
 function fish_right_prompt
 	if [ "$CMD_DURATION" -ge "1000" ]
-            printf (math "floor($CMD_DURATION / 1000)")s
-        end
+        printf "\x1b[33m\x1b[3m"(math "floor($CMD_DURATION / 1000)")"s\x1b[0m"
+    end
 end
