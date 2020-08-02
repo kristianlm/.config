74d313c98e5f4efecbc7d3d3a84a941885d6576d
Kristian Lein-Mathisen <kristianlein@gmail.com>
2020-08-02 19:06:34 +0200

    fish: add env variable for default theme for 'highlight'
    as in pacman -S highlight

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,10 @@ set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
 set -xU LESS_TERMCAP_ue (printf "\e[0m")
 set -xU LESS_TERMCAP_us (printf "\e[00;32m")
 
+# highlight default theme
+set -xU HIGHLIGHT_OPTIONS '-s rootwater'
+
 if [ (tty) = '/dev/tty1' ]
 	exec startx
 end
+
