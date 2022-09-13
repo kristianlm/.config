d873255ca8d311abe0d3aa0b595b5703477e8825
Kristian Lein-Mathisen <kristianlein@gmail.com>
2022-09-13 14:14:43 +0200

    format: fix indentation

--- system.scm
+++ system.scm
@@ -30,140 +30,144 @@
 (define kl (keyboard-layout "us" "mac" #:options '("ctrl:nocaps")))
 
 (operating-system
- (kernel-arguments
-  (cons* "spectre_v2=eibrs,retpoline" ;; mitigate spectre v2 eBPF vulnerability
-         ;; https://wiki.archlinux.org/title/Intel_graphics#Crash/freeze_on_low_power_Intel_CPUs   
-         "intel_idle.max_cstate=1"  ;; CPU power states
-         "i915.enable_dc=0"	    ;; GPU power management
-         "ahci.mobile_lpm_policy=1" ;; SATA power management
-         %default-kernel-arguments))
- (locale "en_US.utf8")
- (timezone "Europe/Oslo")
- (keyboard-layout kl)
- (host-name "pal")
+  (kernel-arguments
+   (cons* "spectre_v2=eibrs,retpoline" ;; mitigate spectre v2 eBPF vulnerability
+          ;; https://wiki.archlinux.org/title/Intel_graphics#Crash/freeze_on_low_power_Intel_CPUs
+          "intel_idle.max_cstate=1" ;; CPU power states
+          "i915.enable_dc=0"        ;; GPU power management
+          "ahci.mobile_lpm_policy=1" ;; SATA power management
+          %default-kernel-arguments))
+  (locale "en_US.utf8")
+  (timezone "Europe/Oslo")
+  (keyboard-layout kl)
+  (host-name "pal")
 
- (users (cons* (user-account
-                (name "klm")
-                (comment "John Doe")
-                (group "users")
-                (home-directory "/home/klm")
-		(shell (file-append fish "/bin/fish"))
-                (supplementary-groups
-                 '("wheel" "netdev" "audio" "video" "disk" "floppy" "cdrom" "kvm" "input")))
-               %base-user-accounts))
- 
- (sudoers-file (plain-file "sudoers" "\
+  (users (cons* (user-account
+                 (name "klm")
+                 (comment "John Doe")
+                 (group "users")
+                 (home-directory "/home/klm")
+		 (shell (file-append fish "/bin/fish"))
+                 (supplementary-groups
+                  '("wheel" "netdev" "audio" "video" "disk" "floppy" "cdrom" "kvm" "input")))
+                %base-user-accounts))
+
+  (sudoers-file (plain-file "sudoers" "\
 root ALL=(ALL) ALL
 %wheel ALL=NOPASSWD: ALL
 "))
- 
- (packages (cons*
-	    btrfs-progs
-	    file
-	    pv
-	    tmux
-	    nss-certs
-	    curl
-	    git-minimal
-   	    %base-packages))
 
- (services
-  (cons*
+  (packages (cons*
+	     btrfs-progs
+	     file
+	     pv
+	     tmux
+	     nss-certs
+	     curl
+	     git-minimal
+   	     %base-packages))
 
-   (service xorg-server-service-type
-	    (xorg-configuration
-	     (fonts
-	      (cons*
-	       font-gnu-unifont
-	       font-terminus
-	       %default-xorg-fonts))
-	     (keyboard-layout kl)))
+  (services
+   (cons*
 
-   (service dhcp-client-service-type)
+    (service xorg-server-service-type
+	     (xorg-configuration
+	      (fonts
+	       (cons*
+	        font-gnu-unifont
+	        font-terminus
+	        %default-xorg-fonts))
+	      (keyboard-layout kl)))
 
-   (service cups-service-type
-            (cups-configuration
-             (web-interface? #t)
-             (extensions (list cups-filters hplip))))
-   
-   (service openssh-service-type
-	    (openssh-configuration
-	     (openssh openssh-sans-x)
-	     (password-authentication? #false)))
+    (service dhcp-client-service-type)
 
-   ;; (service gdm-service-type
-   ;;          (gdm-configuration
-   ;;           (auto-login-user "klm")
-   ;;           (auto-login-session "i3")))
+    (service cups-service-type
+             (cups-configuration
+              (web-interface? #t)
+              (extensions (list cups-filters hplip))))
 
-   ;; (modify-services %desktop-services (delete gdm-service-type))
+    (service openssh-service-type
+	     (openssh-configuration
+	      (openssh openssh-sans-x)
+	      (password-authentication? #false)))
 
-   (service avahi-service-type)
-   ;; (udisks-service)
-   ;; (service upower-service-type)
-   ;; (accountsservice-service)
-   (service cups-pk-helper-service-type)
-   ;; (service colord-service-type)
-   ;; (geoclue-service)
-   (service polkit-service-type)
-   ;; (elogind-service)
-   (dbus-service)
+    ;; (service gdm-service-type
+    ;;          (gdm-configuration
+    ;;           (auto-login-user "klm")
+    ;;           (auto-login-session "i3")))
 
-   ;; (service ntp-service-type)
+    ;; (modify-services %desktop-services (delete gdm-service-type))
 
-   x11-socket-directory-service
+    (service avahi-service-type)
+    ;; (udisks-service)
+    ;; (service upower-service-type)
+    ;; (accountsservice-service)
+    (service cups-pk-helper-service-type)
+    ;; (service colord-service-type)
+    ;; (geoclue-service)
+    (service polkit-service-type)
+    ;; (elogind-service)
+    (dbus-service)
 
-   (service pulseaudio-service-type)
-   (service alsa-service-type)
+    ;; (service ntp-service-type)
 
-   (service mingetty-service-type (mingetty-configuration (tty "tty1")))
-   (service mingetty-service-type (mingetty-configuration (tty "tty2") (auto-login "klm")))
-   (service mingetty-service-type (mingetty-configuration (tty "tty3")))
-   (service mingetty-service-type (mingetty-configuration (tty "tty4")))
-   (service mingetty-service-type (mingetty-configuration (tty "tty5")))
-   (service mingetty-service-type (mingetty-configuration (tty "tty6")))
-   (service mingetty-service-type (mingetty-configuration (tty "tty7")))
-   (service mingetty-service-type (mingetty-configuration (tty "tty8")))
-   (service mingetty-service-type (mingetty-configuration (tty "tty9")))
-   (service mingetty-service-type (mingetty-configuration (tty "tty0")))
-   
-   (modify-services %base-services
-     (delete mingetty-service-type))))
-  
-  
- (mapped-devices (list
-		  (mapped-device
-		   (source "vgf")
-		   (targets (list "vgf-nvm"))
-		   (type lvm-device-mapping))))
+    x11-socket-directory-service
 
- (file-systems
-  (cons* (file-system
-          (mount-point "/")
-          (device "/dev/vgf/nvm")
-	  (dependencies mapped-devices)
-          (type "btrfs"))
+    (service pulseaudio-service-type)
+    (service alsa-service-type)
 
-	 (file-system
-	  (mount-point "/boot/efi")
-	  (device (uuid "73F7-8562" 'fat))
-	  (type "vfat"))
+    (service mingetty-service-type (mingetty-configuration (tty "tty1")))
+    (service mingetty-service-type (mingetty-configuration (tty "tty2") (auto-login "klm")))
+    (service mingetty-service-type (mingetty-configuration (tty "tty3")))
+    (service mingetty-service-type (mingetty-configuration (tty "tty4")))
+    (service mingetty-service-type (mingetty-configuration (tty "tty5")))
+    (service mingetty-service-type (mingetty-configuration (tty "tty6")))
+    (service mingetty-service-type (mingetty-configuration (tty "tty7")))
+    (service mingetty-service-type (mingetty-configuration (tty "tty8")))
+    (service mingetty-service-type (mingetty-configuration (tty "tty9")))
+    (service mingetty-service-type (mingetty-configuration (tty "tty0")))
 
-         (file-system
-	  (mount-point "/raid")
-	  (device (uuid "e2150870-2635-4a01-bcb2-d27dc48a6d9f" 'btrfs))
-          (options "subvol=@bak")
-          (create-mount-point? #t)
-	  (type "btrfs"))         
+    (modify-services %base-services
+      (delete mingetty-service-type))))
 
-         %base-file-systems))
-  
- (bootloader
-  (bootloader-configuration
-   (bootloader grub-efi-bootloader)
-   (terminal-outputs '(console serial))
-   (targets `("/boot/efi"))
-   (keyboard-layout kl)))
+  (hosts-file (plain-file "hosts"
+                          (string-append
+                           (local-host-aliases host-name)
+                           "172.105.68.197 karl")))
 
- (name-service-switch %mdns-host-lookup-nss))
 
+  (name-service-switch %mdns-host-lookup-nss)
+
+  (mapped-devices (list
+		   (mapped-device
+		    (source "vgf")
+		    (targets (list "vgf-nvm"))
+		    (type lvm-device-mapping))))
+
+  (file-systems
+   (cons* (file-system
+            (mount-point "/")
+            (device "/dev/vgf/nvm")
+	    (dependencies mapped-devices)
+            (type "btrfs"))
+
+	  (file-system
+	    (mount-point "/boot/efi")
+	    (device (uuid "73F7-8562" 'fat))
+	    (type "vfat"))
+
+          (file-system
+	    (mount-point "/raid")
+	    (device (uuid "e2150870-2635-4a01-bcb2-d27dc48a6d9f" 'btrfs))
+            (options "subvol=@bak")
+            (create-mount-point? #t)
+	    (type "btrfs"))
+
+          %base-file-systems))
+
+  (bootloader
+   (bootloader-configuration
+    (bootloader grub-efi-bootloader)
+    (terminal-outputs '(console serial))
+    (targets `("/boot/efi"))
+    (keyboard-layout kl))))
