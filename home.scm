8e283b81330dca2438bce3be4f6110c5a3168cb2
Kristian Lein-Mathisen <kristianlein@gmail.com>
2022-10-05 19:38:31 +0200

    guix home: lots of emacs stuff

--- home.scm
+++ home.scm
@@ -57,6 +57,8 @@
      "rlwrap"
      "atool"
      "watchexec"
+     "ncurses" ;; for tput
+     "darkhttpd"
      ;; x
      "sx" "xrandr" "libxft" "xrdb"
      "setxkbmap" "xset"
@@ -66,14 +68,16 @@
      "redshift"
      "font-inconsolata"
      "xdg-utils" ;; <-- xdg-open
+     "maim" "slop" ;; screen capture tools
      ;; doc
      "mupdf" "evince"
      "at-spi2-core" ;; <-- stops annoying org.a11y.Bus warnings in GTK apps
      "graphviz"
      ;; media
      "feh" "imv" "sxiv"
-     "mpv" "ffmpeg"
+     "mpv" "ffmpeg" "sox"
      "inkscape" ;; "solvespace"
+     "pulseaudio"
      ;; code
      "libqalculate"
      "the-silver-searcher"
@@ -90,28 +94,33 @@
      "emacs-all-the-icons-dired"
      "emacs-spaceline-all-the-icons" "emacs-powerline" "emacs-doom-modeline"
      "emacs-selectrum" "emacs-orderless" ;; "emacs-helm" "emacs-ivy"
+     "emacs-ctrlf" "emacs-swiper" ;; i-search alternative
+     "emacs-phi-search" ;; testing
      "emacs-git-gutter-fringe"
      "emacs-highlight-symbol"
      ;; "emacs-idle-highlight" ;; switched to highlight-symbol-mode
      "emacs-magit" "emacs-git-link"
      "emacs-multiple-cursors"
      "emacs-ace-window" "emacs-switch-window" "emacs-frames-only-mode" "emacs-window-purpose"
-     "emacs-paredit" "emacs-smartparens"
+     "emacs-paredit" "emacs-smartparens" "emacs-aggressive-indent"
+     "emacs-vterm"
      "emacs-tagedit"
+     "emacs-dumb-jump"
      "emacs-fish-mode"
      "emacs-web-mode"
      "emacs-json-mode"
      "emacs-js2-mode"
-     "emacs-phi-search" ;; testing
      "emacs-rec-mode"
-     "emacs-aggressive-indent"
-     "emacs-geiser" "emacs-geiser-guile"
+     "emacs-protobuf-mode"
+     ;; "emacs-geiser" "emacs-geiser-guile"
      ;; files / disk
      "rclone" "restic"
      "parted" "lsof"
+     "sqlite"
      ;; system
      "dstat" "htop"
-     "qemu"
+     "qemu" "iperf"
+     ;; evil
      "ungoogled-chromium"))))
 
  (services
@@ -133,6 +142,33 @@ set -xU LESS_TERMCAP_me (printf \"\\e[0m\")
 set -xU LESS_TERMCAP_se (printf \"\\e[0m\")
 set -xU LESS_TERMCAP_so (printf \"\\e[01;44;33m\")
 set -xU LESS_TERMCAP_ue (printf \"\\e[0m\")
+
+# https://github.com/akermu/emacs-libvterm
+if [ \"$INSIDE_EMACS\" = 'vterm' ]
+    function clear
+        vterm_printf \"51;Evterm-clear-scrollback\";
+        tput clear;
+    end
+
+    function fish_title
+        hostname
+        echo \":\"
+        pwd
+    end
+
+    function vterm_prompt_end;
+        vterm_printf '51;A'(whoami)'@'(hostname)':'(pwd)
+    end
+    functions --copy fish_prompt vterm_old_fish_prompt
+    function fish_prompt --description 'Write out the prompt; do not replace this. Instead, put this at end of your file.'
+        # Remove the trailing newline from the original prompt. This is done
+        # using the string builtin from fish, but to make sure any escape codes
+        # are correctly interpreted, use %b for printf.
+        printf \"%b\" (string join \"\n\" (vterm_old_fish_prompt))
+        vterm_prompt_end
+    end
+end
+
 if [ (tty) = '/dev/tty2' ]
   exec sx
 end
