cacd81ec01a97b8b8d34bf33c5edb2ffbe3c476f
Kristian Lein-Mathisen <kristianlein@gmail.com>
2023-04-19 14:57:28 +0200

    pal: fix pavucontrol settings
    for speaker output

--- bin/pa-toggle-profile.fish
+++ bin/pa-toggle-profile.fish
@@ -12,7 +12,7 @@ end
 
 if toggling
     if headphones
-        pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo-extra1+input:analog-stereo
+        pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo+input:analog-stereo
     else
         pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
     end
