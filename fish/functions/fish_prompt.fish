87675550e2483626a430d13e914688415874ad8c
Kristian Lein-Mathisen <kristianlein@gmail.com>
2023-05-09 10:43:53 +0200

    misc: experiments with guix home

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,15 +1,20 @@
 function fish_prompt
+    set mystatus $status
+
+    printf (set_color -o yellow)
+    if set -q GUIX_PROFILE
+        printf "[$(basename $GUIX_PROFILE)] "
+    else if set -q GUIX_ENVIRONMENT
+        printf "[env] "
+    end
+
     # color prompt by exit status
-    if test $status -le 0
+    if test $mystatus -le 0
         printf (set_color -o purple)
     else
 	printf (set_color -o red)
     end
 
     printf "$USER@"(prompt_hostname)" "(prompt_pwd)" "
-    if set -q GUIX_ENVIRONMENT
-        printf "[env] "(set_color normal)
-    else
         printf "➤ "(set_color normal)
-    end
 end
